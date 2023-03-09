69c2158c5556e93c0482bfd3ca2a87b1b3a5f818
KieranTou <qydou7@gmail.com>
2023-03-09 15:35:57 +0800

    0309

--- demo/project/createCard/urls.py
+++ demo/project/createCard/urls.py
@@ -1,9 +1,11 @@
 # 引入path
 from django.urls import path
+from . import views
 
 # 正在部署的应用的名称
 app_name = 'createCard'
 
 urlpatterns = [
     # 目前还没有urls
+    path('createCard_set/', views.createCard_set, name='createCard_set'),
 ]
--- demo/project/createCard/views.py
+++ demo/project/createCard/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 # Create your views here.
+
+# 导入 HttpResponse 模块
+from django.http import HttpResponse
+
+# 视图函数
+
+
+def createCard_set(request):  # request为网页发送的请求
+    return HttpResponse("Hello World!")
